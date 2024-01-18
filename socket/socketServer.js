338ac68dc4524278829bbf729b15a8d704cd0cf1
Md-Asikur <asikurrahaman997@gamil.com>
2024-01-18 16:51:00 +0600

    d

--- socket/socketServer.js
+++ socket/socketServer.js
@@ -21,7 +21,7 @@ const roomInitializeConnectionHandler = require("../socketControllers/room/roomI
 const createSocketServer = (server) => {
     const io = socket(server, {
         cors: {
-            origin: ["http://localhost:3000", "https://talkhouse-tv.netlify.app"],
+            origin: "*",
             methods: ["GET", "POST"],
         },
     });
